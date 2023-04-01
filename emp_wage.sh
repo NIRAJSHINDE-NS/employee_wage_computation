7f7ff0b1e284efcb16076d066915129b8384f9ca
NIRAJ <niraj@rajniraj.com>
2023-04-01 16:14:17 +0530

    [Niraj] Add :This is a script to calulate wage for certain hours

--- emp_wage.sh
+++ emp_wage.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
-#attendence check and wage calculation correspondingly
-attendence=$(( RANDOM % 3 ))
-case $attendence in 
-    0)
-        echo "Employee is absent"
-        no_of_hrs_per_day=0
-        ;;
-    1)
-        echo "Employee is full time present"
-        no_of_hrs_per_day=8
-        ;;
-    2)
-        echo "Employee is part time present"
-        no_of_hrs_per_day=4
-        ;;
-esac
-
 wage_per_hr=20
-wage_per_day=$(( wage_per_hr * no_of_hrs_per_day ))
-no_of_days_per_month=20
-wage_per_month=$(( wage_per_day * no_of_days_per_month ))
-echo "The Wage of the employee for the month is $wage_per_month"
+max_hrs=100
+max_days=20
+total_working_hrs=0
+total_working_days=0
+while(( total_working_hrs < max_hrs && total_working_days < max_days ))
+do
+    (( total_working_days++ ))
+    emp_attendence=$(( RANDOM % 3 ))
+    case $emp_attendence in 
+        0)
+            echo "Employee is absent"
+            no_of_hrs_per_day=0
+            ;;
+        1)
+            echo "Employee is full time present"
+            no_of_hrs_per_day=8
+            ;;
+        2)
+            echo "Employee is part time present"
+            no_of_hrs_per_day=4
+            ;;
+    esac
+        (( total_working_hrs += no_of_hrs_per_day ))
+    
+done
+
+total_salary=$(( total_working_hrs * wage_per_hr ))
+echo "Total salary is $total_salary"
