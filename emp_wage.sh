9b7485f613ca8826c4940193bac8bba491151c6e
NIRAJ <niraj@rajniraj.com>
2023-04-01 16:04:19 +0530

    [Niraj] Add :This is a script to calculate monthly wage

--- emp_wage.sh
+++ emp_wage.sh
@@ -18,4 +18,14 @@ esac
 
 wage_per_hr=20
 wage_per_day=$(( wage_per_hr * no_of_hrs_per_day ))
-echo "The daily wage of the employee is $wage_per_day "
+no_of_days_per_month=20
+wage_per_month=$(( wage_per_day * no_of_days_per_month ))
+echo "The Wage of the employee for the month is $wage_per_month"
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
