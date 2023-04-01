02275ec271b952fd573830892ec1fc95a989ac2d
NIRAJ <niraj@rajniraj.com>
2023-04-01 16:08:32 +0530

    [Niraj] Add :This is a script to find monthly wage of employee

--- emp_wage.sh
+++ emp_wage.sh
@@ -21,11 +21,3 @@ wage_per_day=$(( wage_per_hr * no_of_hrs_per_day ))
 no_of_days_per_month=20
 wage_per_month=$(( wage_per_day * no_of_days_per_month ))
 echo "The Wage of the employee for the month is $wage_per_month"
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
