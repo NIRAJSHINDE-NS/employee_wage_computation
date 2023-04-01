21933e46ee36b97c9f20bdad31af5e5c3d46e939
NIRAJ <niraj@rajniraj.com>
2023-04-01 16:17:21 +0530

    [Niraj] Add :This is a script using fuction to calculate work hours

--- emp_wage.sh
+++ emp_wage.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+get_work_hrs ()
+{   
+    emp_attendence=$1
+     case $emp_attendence in 
+        0)
+            no_of_hrs_per_day=0
+            ;;
+        1)
+            no_of_hrs_per_day=8
+            ;;
+        2)
+            no_of_hrs_per_day=4
+            ;;
+    esac
+    echo "$no_of_hrs_per_day"
+}
+
 wage_per_hr=20
 max_hrs=100
 max_days=20
@@ -8,21 +25,8 @@ while(( total_working_hrs < max_hrs && total_working_days < max_days ))
 do
     (( total_working_days++ ))
     emp_attendence=$(( RANDOM % 3 ))
-    case $emp_attendence in 
-        0)
-            echo "Employee is absent"
-            no_of_hrs_per_day=0
-            ;;
-        1)
-            echo "Employee is full time present"
-            no_of_hrs_per_day=8
-            ;;
-        2)
-            echo "Employee is part time present"
-            no_of_hrs_per_day=4
-            ;;
-    esac
-        (( total_working_hrs += no_of_hrs_per_day ))
+    no_of_hrs_per_day=$( get_work_hrs $emp_attendence)
+    (( total_working_hrs += no_of_hrs_per_day ))
     
 done
 
