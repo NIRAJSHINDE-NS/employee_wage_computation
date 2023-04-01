1377d9c5ae02baf683c7bc5be7f0361ba7da5033
NIRAJ <niraj@rajniraj.com>
2023-04-01 16:01:16 +0530

    [Niraj] Add :This is a script using switch case

--- emp_wage.sh
+++ emp_wage.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 #attendence check and wage calculation correspondingly
 attendence=$(( RANDOM % 3 ))
-if(( attendence == 0 ))
-then
-    echo "Employee is absent"
-    no_of_hrs_per_day=0
-elif(( attendence == 1))
-then
-    echo "Employee is full time present"
-    no_of_hrs_per_day=8
-else
-    echo "Employee is part time present"
-    no_of_hrs_per_day=4
-fi
+case $attendence in 
+    0)
+        echo "Employee is absent"
+        no_of_hrs_per_day=0
+        ;;
+    1)
+        echo "Employee is full time present"
+        no_of_hrs_per_day=8
+        ;;
+    2)
+        echo "Employee is part time present"
+        no_of_hrs_per_day=4
+        ;;
+esac
 
 wage_per_hr=20
 wage_per_day=$(( wage_per_hr * no_of_hrs_per_day ))
